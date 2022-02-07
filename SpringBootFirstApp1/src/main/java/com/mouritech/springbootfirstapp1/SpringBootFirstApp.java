6e2d646900513d5d8b51b0a657dc9b663b716790
Rupa Polipilli <96765255+RupaPolipilliMouri@users.noreply.github.com>
2022-02-07 17:36:37 +0530

    SpringBoot FirstApp creation by using IO web with dependencies

---
+++ SpringBootFirstApp1/src/main/java/com/mouritech/springbootfirstapp1/SpringBootFirstApp.java
@@ -0,0 +1,15 @@
+package com.mouritech.springbootfirstapp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFirstApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFirstApp.class, args);
+		
+		System.out.println("My FirstSpring Boot App by using io web");
+	}
+
+}
---
+++ SpringBootFirstApp1/src/test/java/com/mouritech/springbootfirstapp1/SpringBootFirstApp1/SpringBootFirstApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mouritech.springbootfirstapp1.SpringBootFirstApp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFirstApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
