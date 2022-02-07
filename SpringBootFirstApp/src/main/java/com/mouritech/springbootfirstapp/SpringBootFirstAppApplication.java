0ca924190eaed399501a524ae823873a866ed903
Rupa Polipilli <96765255+RupaPolipilliMouri@users.noreply.github.com>
2022-02-07 17:23:14 +0530

    SpringBootFirstApp without dependencies

---
+++ SpringBootFirstApp/src/main/java/com/mouritech/springbootfirstapp/SpringBootFirstAppApplication.java
@@ -0,0 +1,15 @@
+package com.mouritech.springbootfirstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.mysql.cj.x.protobuf.MysqlxDatatypes.Scalar.String;
+
+@SpringBootApplication
+public class SpringBootFirstAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFirstAppApplication.class, args);
+	}
+
+}
